01141842ee5d9ae929767a0422a6ede7adab4864
mak017 <makovskyy0983@gmail.com>
2019-06-21 21:13:27 +0300

    Corrections 1

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('[data-go-btn]').addEventListener('click', function() {
-    document.querySelector('.screen1').setAttribute('hidden', '');
-    document.querySelector('.screen2').removeAttribute('hidden');
-    document.querySelector('[data-header-counter').setAttribute('hidden', '');
+  const goButtonElem = document.querySelector('[data-go-btn]');
+  if (goButtonElem) {
+    goButtonElem.addEventListener('click', function() {
+      document.querySelector('.screen1').setAttribute('hidden', '');
+      document.querySelector('.screen2').removeAttribute('hidden');
+      document.querySelector('[data-header-counter').setAttribute('hidden', '');
+    });
+  }
+  // Fix iOS viewport bug
+  const fixHeight = function() {
+    document.body.style.height = window.innerHeight + 'px';
+  };
+  fixHeight();
+  window.addEventListener('resize', function() {
+    fixHeight();
   });
 });
