1a952b077e59f816cdfb6177f50f8a4c024aa44b
mak017 <makovskyy0983@gmail.com>
2019-06-25 22:29:32 +0300

    Added socials

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   const goButtonElem = document.querySelector('[data-go-btn]');
+  const shareButtonElem = document.querySelector('[data-share-btn]');
   if (goButtonElem) {
     goButtonElem.addEventListener('click', function() {
       document.querySelector('.screen1').setAttribute('hidden', '');
       document.querySelector('.screen2').removeAttribute('hidden');
       document.querySelector('[data-header-counter').setAttribute('hidden', '');
     });
+  }
+  if (shareButtonElem) {
+    shareButtonElem.addEventListener('click', function() {
+      document.querySelector('[data-img-container-overlay]').removeAttribute('hidden')
+    })
   }
   // Fix iOS viewport bug
   const fixHeight = function() {
